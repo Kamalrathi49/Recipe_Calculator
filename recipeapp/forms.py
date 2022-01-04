a3624f40f935359bce2b036b0c54ce8308e31721
Kamal Rathi <kamalrathi049@gmail.com>
2022-01-04 14:28:04 +0530

    updated

--- recipeapp/forms.py
+++ recipeapp/forms.py
@@ -29,17 +29,17 @@ class LoginForm(forms.Form):
 
 
 class RegistrationForm(forms.Form):
+    username = forms.CharField(max_length=225, widget=forms.TextInput(
+        attrs={'placeholder': 'Username'}))
     first_name = forms.CharField(max_length=225, widget=forms.TextInput(
         attrs={'placeholder': 'FirstName'}))
     last_name = forms.CharField(max_length=225, widget=forms.TextInput(
         attrs={'placeholder': 'LastName'}))
-    username = forms.CharField(max_length=225, widget=forms.TextInput(
-        attrs={'placeholder': 'Username'}))
-    password = forms.CharField(max_length=225, widget=forms.PasswordInput(
-        attrs={'placeholder': 'Password'}))
     email = forms.EmailField(max_length=225, widget=forms.EmailInput(
         attrs={'placeholder': 'Email'}
     ))
+    password = forms.CharField(max_length=225, widget=forms.PasswordInput(
+        attrs={'placeholder': 'Password'}))
 
 
 class UpdateEmailForm(forms.ModelForm):
